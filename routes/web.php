9ddf3204100ef43d91d561afcb4a028e4f538419
iago holek <87425168+FunctionTestIfImGood@users.noreply.github.com>
2022-11-30 22:34:04 -0300

    operation 4 - rota com parametros

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::get('/',[\App\Http\Controllers\PrincipalController::class, 'print']);
 Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class, 'print']);
 
 Route::get('/secao-um',[\App\Http\Controllers\SecaoUmController::class, 'print']);
+
+Route::get('/contato/{nome}', function (String $nome){
+    echo 'estamos aqui ' .$nome;
+}
+);
+
+Route::get('/contato', function (){
+    echo 'estamos aqui';
+}
+);
